20fce127960a3c76967b8a14cd5d06256d8bdb6a
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-04-12 11:07:12 +0530

    Final commit

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const sounds = ['bababoey','bonk','bom','bruh','c1','c2','c3','c4','c5','c6','c7','shine','wow','yahoo'];
+const sounds = ['bababoey','bonk','bomm','bruh','Cod-1','Cod-2','Cod-3','Cod-4','Cod-5','Cod-6','Cod-7','shine','wow','yahoo'];
 
 sounds.forEach(sound => {
     const btn = document.createElement('button');
     btn.classList.add('btn');
+
+
+    btn.addEventListener('click',()=>{
+        document.getElementById(sound).play()
+    })
 
     btn.innerText=sound;
     document.getElementById('buttons').appendChild(btn);
